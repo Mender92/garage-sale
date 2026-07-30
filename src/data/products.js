c98f9cd6b8a747a9a28964979b01911f1f551f4f
Domen Kjuder <KjuderD@Doktor24.local>
2026-07-30 08:48:31 +0200

    Add nicotine pouches to garage sale

--- src/data/products.js
+++ src/data/products.js
@@ -1,5 +1,6 @@
 import caseImage from "../assets/images/case.jpeg";
 import guitarImage from "../assets/images/guitar.jpeg";
+import snusImage from "../assets/images/snus.jpeg";
 
 const products = [
   {
@@ -17,6 +18,14 @@ const products = [
     image: caseImage,
     description:
       "Well-maintained travel suitcase in good condition. Spacious, lightweight, and easy to maneuver thanks to its smooth-rolling wheels. Perfect for vacations, business trips, or weekend getaways. Fully functional with normal signs of use.",
+  },
+  {
+    id: 3,
+    title: "Outdare Nicotine Pouches",
+    price: "€5/can",
+    image: snusImage,
+    description:
+      "Accidentally purchased nicotine pouches instead of energy pouches. The price is 5€/can when buying all 20 cans together. Otherwise, the price is 6€/can. All cans are unopened and in perfect condition.",
   },
 ];
 
